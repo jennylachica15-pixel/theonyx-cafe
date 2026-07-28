3b538af151d5933237dc1834f7d0763592e84824
jennylachica15-pixel <jennylachica15@gmail.com>
2026-07-28 16:54:45 +0800

    Add 'BREAKFAST PANCAKE' to Orders menu

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -87,6 +87,7 @@ const MENU_GROUPS = [
     { name: 'COOKIES SMALL',        regular: 35 },
     { name: 'SWEET BITES',          regular: 15 },
     { name: 'GRILLED CHEESE SANDWICH', regular: 80 },
+    { name: 'BREAKFAST PANCAKE', regular: 80 },
   ]},
   { id: 'snacks', label: 'Snacks', items: [
     { name: 'CHEESE BURGER',   regular: 50  },
