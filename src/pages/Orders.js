d68ff6661402d84070f090f895ac5f714657dce3
jennylachica15-pixel <jennylachica15@gmail.com>
2026-07-28 16:38:19 +0800

    Add Chicken Wings options to Orders

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -75,6 +75,8 @@ const MENU_GROUPS = [
     { name: 'RICE MEAL - C. PEPPER STEAK', regular: 180 },
     { name: 'RICE MEAL - C. KOREAN',  regular: 180 },
     { name: 'RICE MEAL - C. INASAL',  regular: 180 },
+    { name: 'Chicken Wings-Buffalo', regular: 190 },
+    { name: 'Chicken Wings-Salted Egg', regular: 190 },
   ]},
   { id: 'pastries', label: 'Pastries & sweets', items: [
     { name: 'WAFFLE - MANGO',       regular: 80 },
